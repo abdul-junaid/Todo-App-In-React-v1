cb4e16d0fbf88541f1a07c24ae7c254cce5e26bd
Abdul Junaid <junaididavazhikkal@gmail.com>
2021-05-23 17:58:09 +0530

    form created

---
+++ src/components/form/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+    render() {
+        return (
+            <div className="form-section">
+                <form autoComplete="off">
+                    <h1>TODO APP</h1>
+                    <div className="input-section">
+                        <input type="text" placeholder="Enter Item" />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
